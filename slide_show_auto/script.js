c39f56d191b08e5b43412721d0df5e2d48571ec8
LionyxML <rahul.juliato@gmail.com>
2021-04-05 16:54:09 -0300

    changed core animation with toggle instead of add/remove. added shadow

--- slide_show_auto/script.js
+++ slide_show_auto/script.js
@@ -1,49 +1,44 @@
-var images = [
+var imagesList = [
   './img/1.jpg',
   './img/2.jpg',
   './img/3.jpg',
   './img/4.jpg'
 ];
+
 var i = 0;
 var time = 3000;
 var img = document.getElementsByClassName('img100');
+var slide = document.getElementsByClassName('slides')[0];
+
 
 function changeImage() {
 
-  if (i < images.length - 1){
-    img[i].classList.remove("shown");
-    img[i].classList.add("hidden");
-    img[i+1].classList.remove("hidden");
-    img[i+1].classList.add("shown");
+  if (i < imagesList.length - 1){
+    img[i].classList.toggle("shown");
+    img[i+1].classList.toggle("shown");
     i++;
   } else {
-    img[i].classList.add("hidden");
-    img[i].classList.remove("shown");
+    img[i].classList.toggle("shown");
     i = 0;
-    img[i].classList.add("shown");
-    img[i].classList.remove("hidden");
+    img[i].classList.toggle("shown");
   }
 
   setTimeout("changeImage()", time);
 }
 
 
-
-var slide = document.getElementsByClassName('slides')[0];
-
 function populateImages () {
   var imgSkel =`<img class="img100 shown" src="###" alt="">`;
 
-  images.forEach((item, i) => {
-    newImg = imgSkel.replace('###', images[i]);
-    if (i > 0) newImg = newImg.replace('shown', 'hidden');
+  imagesList.forEach((item, i) => {
+    newImg = imgSkel.replace('###', imagesList[i]);
+    if (i > 0) newImg = newImg.replace('shown', '');
     slide.insertAdjacentHTML('beforeEnd', newImg);
   });
-
 }
+
 
 window.onload = function () {
   populateImages();
   changeImage();
-
 };
